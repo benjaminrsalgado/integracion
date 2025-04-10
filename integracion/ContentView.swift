54ca46bd7927dae5b98ed2ffb8bce1393c4dae6f
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-04-10 16:30:51 -0600

    i add url images

--- integracion/ContentView.swift
+++ integracion/ContentView.swift
@@ -12,13 +12,54 @@ struct ContentView: View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.red)
+            Text("Hello,WWDC!")
+            AsyncImage(url: URL(string: "https://www.apple.com/newsroom/images/2025/03/apples-worldwide-developers-conference-returns-the-week-of-june-9/tile/Apple-WWDC24-event-branding-lp.jpg.landing-big_2x.jpg")) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                Text("Cargando imagen...")
+                    .foregroundColor(.red)
+                    .font(.largeTitle)
+            }
+            .frame(width: 500, height: 200)
+            
         }
         .padding()
     }
 }
+import SwiftUI
+
+struct PruebaView: View {
+    var body: some View {
+        AsyncImage(url: URL(string: "https://images.macrumors.com/article-new/2025/02/WWDC-25-Feature-2.jpg")) { phase in
+            switch phase {
+            case .empty:
+                Text("Cargando imagen...")
+                
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFit()
+                
+            case .failure(_):
+                Text("❌ Error al cargar la imagen")
+                
+            @unknown default:
+                EmptyView()
+                
+                
+            }
+            
+        }
+        
+        .frame(width: 600, height: 200)
+    }
+   
+}
 
 #Preview {
     ContentView()
+    PruebaView()
 }
